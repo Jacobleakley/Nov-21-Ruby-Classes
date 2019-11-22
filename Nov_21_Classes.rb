3507acb71edfffb30617aa5005d8ed432b277f43
Jacobleakley <jacob.bleakley@gmail.com>
2019-11-21 21:47:20 -0500

    Ruby Classes 1st exercise

---
+++ Nov_21_Classes.rb
@@ -0,0 +1,55 @@
+#Get<attribute> >>>> puts or prints <Object>.Get<attribute> --> [Resultant Value]
+
+
+
+
+class Animal
+    def initialize
+    @height = height
+    @weight = weight
+    @legs = legs
+end
+
+    def grow 
+        @height * 2
+    end
+
+
+    def setLegs(toes)
+    @legs = toes 
+
+
+    def getWeight 
+        return @weight
+
+tiger = Animal.new
+
+
+
+
+
+
+
+
+
+class "Person" 
+    def initialize(mustache, kippa, shoes)
+    @mustache = mustache
+    @kippa = kippa
+    @shoes = shoes
+end
+
+
+
+Jew = Person.new(beard, black, crocks) 
+
+class Building 
+    def initialize(cost, value, bricks)
+    @cost = cost
+    @value = value
+    @bricks = bricks
+end 
+
+Skyscraper = Building.new(250, 100, 25000000)
+
+
